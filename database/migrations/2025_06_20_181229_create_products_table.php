b9497157b56964b38caee459fe91465453fa106f
poly1997p <poly20221997@gmail.com>
2025-06-21 01:23:55 +0600

    class 68

---
+++ database/migrations/2025_06_20_181229_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->unsignedBigInteger('cat_id');
+            $table->unsignedBigInteger('sab_cat_id');
+            $table->double('regular_price');
+            $table->double('discount_price')-> nullable();
+            $table->double('buying_price');
+            $table->integer('qty');
+            $table->string('sku_code')->unique();
+            $table->string('product_type');
+            $table->longText('description');
+            $table->longText('product_policy');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
