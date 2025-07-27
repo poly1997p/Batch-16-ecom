383403f62d2186256b402dd6ced701f4427e0953
poly1997p <poly20221997@gmail.com>
2025-07-28 02:23:22 +0600

    class 80

--- resources/views/frontend/checkout.blade.php
+++ resources/views/frontend/checkout.blade.php
@@ -1,68 +1,105 @@
 @extends('frontend.master')
 
 @section('content')
-   <section class="checkout-section">
-            <div class="container">
-                <form action="" method="post" class="form-group billing-address-form" enctype="multipart/form-data">
-                    <div class="row">
-                        <div class="col-lg-8 col-md-6">
-                            <div class="checkout-wrapper">
-                                <div class="billing-address-wrapper">
-                                    <h4 class="title">Billing / Shipping Details</h4>
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <input type="text" name="name" class="form-control" placeholder="Enter Full Name *"/>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <input type="text" name="phone" class="form-control" placeholder="Phone *"/>
-                                        </div>
-                                        <div class="col-md-12">
-                                            <textarea rows="4" name="address" class="form-control" id="address"
-                                                placeholder="Enter Full Address"></textarea>
-                                        </div>
-                                        <div class="col-md-12 mt-3">
+    <section class="checkout-section">
+        <div class="container">
+            <form action="" method="post" class="form-group billing-address-form" enctype="multipart/form-data">
+                <div class="row">
+                    <div class="col-lg-8 col-md-6">
+                        <div class="checkout-wrapper">
+                            <div class="billing-address-wrapper">
+                                <h4 class="title">Billing / Shipping Details</h4>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <input type="text" name="name" class="form-control"
+                                            placeholder="Enter Full Name *" />
+                                    </div>
+                                    <div class="col-md-6">
+                                        <input type="text" name="phone" class="form-control" placeholder="Phone *" />
+                                    </div>
+                                    <div class="col-md-12">
+                                        <textarea rows="4" name="address" class="form-control" id="address" placeholder="Enter Full Address"></textarea>
+                                    </div>
+
+                                    @php
+                                        $totalPrice = 0;
+                                    @endphp
+
+                                    @foreach ($cartProducts as $cart)
+                                        @php
+                                            $totalPrice = $totalPrice + $cart->qty * $cart->price;
+                                        @endphp
+                                    @endforeach
+
+                                    <div class="col-md-12 mt-3">
+                                        @if ($totalPrice >= 60000)
                                             <div style="background: lightgrey;padding: 10px;margin-bottom: 10px;">
-                                                <input type="radio" id="inside_dhaka" name="area" value="80"/>
+                                                <input type="radio" id="inside_dhaka" name="area" checked
+                                                    value="00" onclick="" />
+                                                <label for="inside_dhaka"
+                                                    style="font-size: 18px;font-weight: 600;color: #000;">Free Delivery (0
+                                                    Tk.)</label>
+                                            </div>
+                                        @else
+                                            <div style="background: lightgrey;padding: 10px;margin-bottom: 10px;">
+                                                <input type="radio" id="inside_dhaka" name="area" checked
+                                                    value="80" onclick="insideDhakaCharge()" />
                                                 <label for="inside_dhaka"
                                                     style="font-size: 18px;font-weight: 600;color: #000;">Inside Dhaka (80
                                                     Tk.)</label>
                                             </div>
                                             <div style="background: lightgrey;padding: 10px;">
-                                                <input type="radio" id="outside_dhaka" name="area" value="150"/>
+                                                <input type="radio" id="outside_dhaka" name="area" checked
+                                                    value="150" onclick="insideDhakaCharge()" />
                                                 <label for="outside_dhaka"
                                                     style="font-size: 18px;font-weight: 600;color: #000;">Outside Dhaka (150
                                                     Tk.)</label>
                                             </div>
-                                        </div>
+                                        @endif
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-4 col-md-6">
-                            <div class="checkout-items-wrapper">
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="checkout-items-wrapper">
+                            @php
+                                $totalPrice = 0;
+                            @endphp
+                            @foreach ($cartProducts as $cart)
+                                @php
+                                    $totalPrice = $totalPrice + $cart->qty * $cart->price;
+                                @endphp
+
                                 <div class="checkout-item-outer">
                                     <div class="checkout-item-left">
                                         <div class="checkout-item-image">
-                                            <img src="{{asset('/frontend/images/product.png')}}" alt="Image"/>
+                                            <img src="{{ asset('/backend/images/product/' . $cart->product->image) }}"
+                                                alt="Image" />
                                         </div>
                                         <div class="checkout-item-info">
                                             <h6 class="checkout-item-name">
-                                                Test Product
+                                                {{ $cart->product->name }}
                                             </h6>
                                             <p class="checkout-item-price">
-                                                300 Tk.
+                                                {{ $cart->price }} tk.
                                             </p>
                                             <span class="checkout-item-count">
-                                                1 item
+                                                {{ $cart->qty }} item
                                             </span>
                                             <br />
-                                            <span class="checkout-item-count">
-                                                Size:                                                 
-                                            </span>                                                
-                                            <span class="checkout-item-count">
-                                                Color: 
-                                            </span>
-                                            {{--<div class="checkout-product-incre-decre">
+                                            @if ($cart->size != null)
+                                                <span class="checkout-item-count">
+                                                    Size: {{ $cart->size }}
+                                                </span>
+                                            @endif
+
+                                            @if ($cart->color != null)
+                                                <span class="checkout-item-count">
+                                                    Color: {{ $cart->color }}
+                                                </span>
+                                            @endif
+                                            {{-- <div class="checkout-product-incre-decre">
                                                 <button type="button" title="Decrement" class="qty-decrement-btn">
                                                     <i class="fas fa-minus"></i>
                                                 </button>
@@ -70,36 +107,49 @@
                                                 <button type="button" title="Increment" class="qty-increment-btn">
                                                     <i class="fas fa-plus"></i>
                                                 </button>                                                
-                                            </div>--}}
+                                            </div> --}}
                                         </div>
                                     </div>
                                     <div class="checkout-item-right">
-                                        <a href="#" class="delete-btn">
+                                        <a href="{{ url('/add-to-cart/delete/' . $cart->id) }}" class="delete-btn">
                                             <i class="fas fa-trash-alt"></i>
                                         </a>
                                     </div>
                                 </div>
-                                <div class="sub-total-wrap">
-                                    <div class="sub-total-item">
-                                         <strong>Sub Total</strong>
-                                        <strong id="subTotal">৳ 300</strong>
-                                    </div>
-                                    <div class="sub-total-item">
+                            @endforeach
+
+                            <div class="sub-total-wrap">
+                                <div class="sub-total-item">
+                                    <strong>Sub Total</strong>
+                                    <strong id="subTotal">৳ {{ $totalPrice }}</strong>
+                                    <input type="hidden" id="inputTotalPrice" name="inputTotalPrice" value="{{$totalPrice}}">
+                                </div>
+                                  <div class="sub-total-item">
                                         <strong>Delivery charge</strong>
-                                        <strong id="deliveryCharge">৳ 80</strong>
+                                        @if ($totalPrice >= 60000)
+                                            <strong id="deliveryCharge">৳ 0</strong>
+                                            @else
+                                            <strong id="deliveryCharge">৳ 80</strong>
+                                        @endif
                                     </div>
                                     <div class="sub-total-item grand-total">
                                          <strong>Grand Total</strong>
-                                         <strong id="grandTotal">৳ 380</strong>
+                                         @if ($totalPrice >= 60000)
+                                             <strong id="grandTotal">৳ {{$totalPrice+0}}</strong>
+                                             <input type="hidden" name="inputGrandTotal" id="inputGrandTotal" value="{{$totalPrice+0}}">
+                                             @else
+                                             <strong id="grandTotal">৳ {{$totalPrice+80}}</strong>
+                                             <input type="hidden" name="inputGrandTotal" id="inputGrandTotal" value="{{$totalPrice+80}}">
+                                         @endif
                                     </div>
-                                </div>
                                 <div class="payment-item-outer">
                                     <h6 class="payment-item-title">
                                         Select Payment Method
                                     </h6>
                                     <div class="payment-items-wrap justify-content-center">
                                         <div class="payment-item-outer">
-                                            <input type="radio" name="payment_type" id="cod" value="cod" checked="">
+                                            <input type="radio" name="payment_type" id="cod" value="cod"
+                                                checked="">
                                             <label class="payment-item-outer-lable" for="cod">
                                                 <strong>Cash On Delivery</strong>
                                             </label>
@@ -115,7 +165,16 @@
                             </div>
                         </div>
                     </div>
-                </form>
-            </div>
-        </section> 
+            </form>
+        </div>
+    </section>
 @endsection
+
+@push('script')
+    <script>
+        function insideDhakaCharge() {
+            document.getElementById('deliveryCharge').innerHTML = "৳ " + 80;
+            let  totalPrice = parseFloat(document.getElementById('inputTotalPrice').value);
+        }
+    </script>
+@endpush
